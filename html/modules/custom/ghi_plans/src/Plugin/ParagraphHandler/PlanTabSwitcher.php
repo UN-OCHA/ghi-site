3bf63e966ac06fda068eceefdef58974ed630c6e
Peter Droogmans (attiks) <peter@attiks.com>
2021-05-17 14:54:23 +0200

    Basic top nav

---
+++ html/modules/custom/ghi_plans/src/Plugin/ParagraphHandler/PlanTabSwitcher.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\ghi_plans\Plugin\ParagraphHandler;
+
+use Drupal\ghi_paragraph_handler\Plugin\ParagraphHandlerBase;
+
+/**
+ * Base class for paragraph handlers.
+ *
+ * @ParagraphHandler(
+ *   id = "plan_tab_switcher",
+ *   label = @Translation("Plan tab switcher")
+ * )
+ */
+class PlanTabSwitcher extends ParagraphHandlerBase {
+
+  /**
+   * Key used for storage
+   */
+  const KEY = 'plan_tab_switcher';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preprocess(array &$variables, array $element) {
+    parent::preprocess($variables, $element);
+    $config = $this->getConfig();
+
+    /** @var \Drupal\node\Entity\Node $plan */
+    $plan = $this->parentEntity;
+    if ($plan->getType() !== 'plan' && $plan->getType() !== 'plan_para' && $plan->getType() !== 'plan_entity') {
+      return;
+    }
+
+    // Get parent if needed.
+    if ($plan->getType() === 'plan_entity') {
+      /** @var \Drupal\node\Entity\Node $plan */
+      $plan = $plan->field_plan->first->entity;
+    }
+
+    $overview_link = t('Overview');
+    if (!$plan->isPublished()) {
+      if ($plan->access('view')) {
+        $overview_link = $plan->toLink($overview_link);
+      }
+    }
+    else {
+      $overview_link = $plan->toLink($overview_link);
+    }
+
+    $tabs = [
+      [
+        'title' => t('Overview'),
+        'url' => $plan->toUrl(),
+        'attributes' => [
+          'class' => ['active'],
+        ],
+      ],
+    ];
+
+    // Get all plan entity nodes with plan as parent.
+    $plan_entities = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties([
+      'type' => 'plan_entity',
+      'field_plan_id' => $plan->id()
+    ]);
+
+    if ($plan_entities) {
+      $suffix = '';
+      if ($config['show_count']) {
+        $suffix = ' <span class="counter">' . count($plan_entities)  . '</span>';
+      }
+
+      $tabs[] = [
+        'title' => t('Clusters') . $suffix,
+        'url' => $plan_entities[0]->toUrl(),
+        'attributes' => [
+          'class' => [],
+        ],
+      ];
+    }
+
+    $variables['content'][] = [
+      '#theme' => 'links',
+      '#links' => $tabs,
+    ];
+  }
+
+  /**
+   * Return behavior settings.
+   */
+  protected function getConfig() {
+    $settings = $this->paragraph->getAllBehaviorSettings();
+    $config = $settings[static::KEY] ?? [
+      'show_count' => TRUE,
+    ];
+
+    return $config;
+  }
+
+}
