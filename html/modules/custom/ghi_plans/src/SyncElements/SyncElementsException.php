7015f67801400fa5900f55e0bf00c3f6419e7dd2
berliner <berliner@dev030.com>
2021-05-25 13:09:40 +0200

    GHI-18: Remove obsolete files

--- html/modules/custom/ghi_plans/src/SyncElements/SyncElementsException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\ghi_plans\SyncElements;
-
-/**
- * Exception thrown when an operation misses a data value; i.e., it is unset.
- */
-class SyncElementsException extends \Exception {}
