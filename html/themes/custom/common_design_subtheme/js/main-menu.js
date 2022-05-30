800da1da0d2346271f44bbff53997d83add95f5f
berliner <berliner@dev030.com>
2022-05-30 22:48:52 +0200

    Add base structure for operations mega menu

---
+++ html/themes/custom/common_design_subtheme/js/main-menu.js
@@ -0,0 +1,7 @@
+(function ($, Drupal) {
+  Drupal.behaviors.GhiMainMenu = {
+    attach: function (context, settings) {
+      // Drupal.behaviors.cdDropdown.updateDrupalTogglableMenus('.cd-nav > .menu > .menu-item > a + .menu-link-contentmain');
+    }
+  };
+}(jQuery, Drupal));
