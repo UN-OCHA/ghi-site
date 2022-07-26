60398b2bbb4cb82813a1ba0ac2734208ee8fb6aa
berliner <berliner@dev030.com>
2022-07-26 02:24:59 +0200

    HPC-8622: Add optional publictions link to document links element

--- html/modules/custom/ghi_blocks/src/Plugin/ConfigurationContainerItem/DocumentLink.php
+++ html/modules/custom/ghi_blocks/src/Plugin/ConfigurationContainerItem/DocumentLink.php
@@ -4,6 +4,7 @@ namespace Drupal\ghi_blocks\Plugin\ConfigurationContainerItem;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\ghi_form_elements\ConfigurationContainerItemPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a document link item for configuration containers.
@@ -17,6 +18,22 @@ use Drupal\ghi_form_elements\ConfigurationContainerItemPluginBase;
 class DocumentLink extends ConfigurationContainerItemPluginBase {
 
   const TITLE_MAX_LENGTH = 50;
+
+  /**
+   * The date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  public $dateFormatter;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->dateFormatter = $container->get('date.formatter');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
@@ -48,6 +65,33 @@ class DocumentLink extends ConfigurationContainerItemPluginBase {
       '#theme' => 'document_link_box',
       '#document' => $document,
     ];
+  }
+
+  /**
+   * Get the date for a document link.
+   *
+   * @return string
+   *   The formatted date of the document link.
+   */
+  public function getFormattedDate() {
+    $document = $this->config['value'];
+    $timestamp = strtotime($document['date']);
+    $date = $this->dateFormatter->format($timestamp, 'custom', 'd M Y');
+    return $date;
+  }
+
+  /**
+   * Get the configured languages for a document link.
+   *
+   * @return string
+   *   The configured languages for a document link, separated by comma.
+   */
+  public function getConfiguredLanguages() {
+    $document = $this->config['value'];
+    $languages = array_keys(array_filter($document['file_details'], function ($details) {
+      return !empty($details['target_url']);
+    }));
+    return implode(', ', $languages);
   }
 
 }
