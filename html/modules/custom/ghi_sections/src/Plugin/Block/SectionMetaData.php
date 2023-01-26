ecab9498186f330ab8191e2a2b882c96b223b0d9
berliner <berliner@dev030.com>
2023-01-26 14:42:33 +0100

    HPC-8882: Move social links to the metadata section, unify rendering of metadata on sections and articles, only show social links on published page nodes

--- html/modules/custom/ghi_sections/src/Plugin/Block/SectionMetaData.php
+++ html/modules/custom/ghi_sections/src/Plugin/Block/SectionMetaData.php
@@ -33,7 +33,7 @@ class SectionMetaData extends BlockBase implements ContainerFactoryPluginInterfa
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    /** @var \Drupal\ghi_subpages\Plugin\Block\SubpageNavigation $instance */
+    /** @var \Drupal\ghi_sections\Plugin\Block\SectionMetaData $instance */
     $instance = new static($configuration, $plugin_id, $plugin_definition);
     $instance->subpageManager = $container->get('ghi_subpages.manager');
     return $instance;
@@ -47,8 +47,18 @@ class SectionMetaData extends BlockBase implements ContainerFactoryPluginInterfa
     if (!$section) {
       return NULL;
     }
+    $metadata = $section->getPageTitleMetaData();
+    if (!$metadata) {
+      return NULL;
+    }
+    if ($this->getPageNode()?->isPublished()) {
+      $metadata[] = [
+        '#theme' => 'social_links',
+      ];
+    }
     return [
-      '#markup' => $section->getPageTitleMetaData(),
+      '#theme' => 'item_list',
+      '#items' => $metadata,
       '#full_width' => TRUE,
     ];
   }
@@ -60,19 +70,29 @@ class SectionMetaData extends BlockBase implements ContainerFactoryPluginInterfa
    *   The current section node or NULL if none can be found.
    */
   private function getSectionNode() {
-    $contexts = $this->getContexts();
-    if (empty($contexts['node']) || !$contexts['node']->hasContextValue()) {
-      return NULL;
-    }
-
-    /** @var \Drupal\node\NodeInterface $node */
-    $node = $contexts['node']->getContextValue();
+    $node = $this->getPageNode();
     $section_node = $this->subpageManager->getBaseTypeNode($node);
     // Let's not handle global sections that are not homepages for the moment.
     if ($section_node instanceof GlobalSection && !$section_node instanceof Homepage) {
       return NULL;
     }
     return $section_node;
+  }
+
+  /**
+   * Get the current page node.
+   *
+   * @return \Drupal\node\NodeInterface|null
+   *   The current page node or NULL if none can be found.
+   */
+  private function getPageNode() {
+    $contexts = $this->getContexts();
+    if (empty($contexts['node']) || !$contexts['node']->hasContextValue()) {
+      return NULL;
+    }
+
+    /** @var \Drupal\node\NodeInterface $node */
+    return $contexts['node']->getContextValue();
   }
 
 }
