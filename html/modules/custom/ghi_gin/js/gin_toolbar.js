5150126ed51fd4a080f33c1b9690fd4be85e5619
berliner <berliner@dev030.com>
2023-04-08 23:20:00 +0200

    HPC-9049: Final touches to bring back the original UI/UX for layout builder into GHI, fix alignments and styles, update layout builder ipe

--- html/modules/custom/ghi_gin/js/gin_toolbar.js
+++ html/modules/custom/ghi_gin/js/gin_toolbar.js
@@ -17,7 +17,16 @@
     attach: (context) => {
       once('glb-button-discard', '.glb-button-discard ').forEach((item)=>{
         item.addEventListener('click', function (event) {
-          document.querySelector('#gin_sidebar .form-actions .glb-button[data-drupal-selector="edit-discard-changes"]').click();
+          // The discard changes button for page manager pages.
+          let cancel_button = document.querySelector('#gin_sidebar .form-actions .glb-button[data-drupal-selector="edit-cancel"]');
+          if (cancel_button) {
+            cancel_button.click();
+          }
+          // The discard changes button for entity pages.
+          let discard_button = document.querySelector('#gin_sidebar .form-actions .glb-button[data-drupal-selector="edit-discard-changes"]');
+          if (discard_button) {
+            discard_button.click();
+          }
         });
       })
     }
