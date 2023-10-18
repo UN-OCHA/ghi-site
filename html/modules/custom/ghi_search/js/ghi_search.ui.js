7de67e896d2a65daa6a667577a8401578088bb02
berliner <berliner@dev030.com>
2023-10-18 14:27:19 +0200

    HPC-9104: Document local solr, add docksal commands for convenience, fix UI issues for auto-open of search form

--- html/modules/custom/ghi_search/js/ghi_search.ui.js
+++ html/modules/custom/ghi_search/js/ghi_search.ui.js
@@ -13,22 +13,30 @@
 
   Drupal.behaviors.GhiSearchUi = {
     attach: function(context, settings) {
-
       let search_form_id = 'block-exposedformsearch-solrpage-search-results';
+      let search_toggle_id = 'block-exposedformsearch-solrpage-search-results-toggler';
       let toggle = $('[aria-controls="' + search_form_id + '"]');
-      $(toggle).once('search-toggle').on('click', function () {
-        if ($(this).attr('aria-expanded') == 'true') {
-          $('body').addClass('search-form-open');
-        }
-        else {
-          $('body').removeClass('search-form-open');
-        }
-      });
 
-      if ($(context).hasClass('path-search') && !$(context).hasClass('search-form-open')) {
+      if ($('body', context).hasClass('path-search') && !$('body', context).hasClass('search-form-open')) {
         $(toggle).trigger('click');
         $('#' + search_form_id).attr('data-cd-hidden', 'false');
         $('body').addClass('search-form-open');
+      }
+
+      if ($(context).find('#' + search_toggle_id)) {
+        var search_toggle = document.getElementById(search_toggle_id);
+        var observer = new MutationObserver(function(mutations) {
+          if ($(search_toggle).attr('aria-expanded') == 'true') {
+            $('body').addClass('search-form-open');
+          }
+          else {
+            $('body').removeClass('search-form-open');
+          }
+        });
+        observer.observe(search_toggle, {
+          attributes: true,
+          attributeFilter: ['aria-expanded']
+        });
       }
     }
   }
