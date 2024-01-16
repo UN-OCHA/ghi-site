4b6f818f7614e0062ec31967f59710eb92a633f2
berliner <berliner@dev030.com>
2024-01-16 10:37:25 +0100

    HPC-9308: Remove chapter/section coming from CM. This is replaced by full document-chapter integration where articles are manually added to document chapters instead of marking an article to be long to a single chapter.

--- html/modules/custom/ghi_content/src/RemoteSource/RemoteSourceBaseHpcContentModule.php
+++ html/modules/custom/ghi_content/src/RemoteSource/RemoteSourceBaseHpcContentModule.php
@@ -84,7 +84,6 @@ abstract class RemoteSourceBaseHpcContentModule extends RemoteSourceBase {
       'id',
       'title',
       'title_short',
-      'section',
       'summary',
       'tags',
       'created',
@@ -486,7 +485,6 @@ abstract class RemoteSourceBaseHpcContentModule extends RemoteSourceBase {
           id
           title
           title_short
-          section
           summary
           created
           updated
