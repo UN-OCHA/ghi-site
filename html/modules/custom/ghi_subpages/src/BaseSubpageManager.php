4a38326ed6dd6119878247e731f0adc70dc7dbd1
berliner <berliner@dev030.com>
2024-03-28 18:36:03 +0100

    HPC-9463: Add some unit tests for the SubpageManager

--- html/modules/custom/ghi_subpages/src/BaseSubpageManager.php
+++ html/modules/custom/ghi_subpages/src/BaseSubpageManager.php
@@ -2,18 +2,20 @@
 
 namespace Drupal\ghi_subpages;
 
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Extension\ModuleHandler;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\ghi_sections\SectionManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Base manager service class.
  */
-abstract class BaseSubpageManager {
+abstract class BaseSubpageManager implements ContainerInjectionInterface {
 
   /**
    * The module handler service.
@@ -67,7 +69,7 @@ abstract class BaseSubpageManager {
   /**
    * Constructs a document manager.
    */
-  public function __construct(ModuleHandler $module_handler, EntityTypeManagerInterface $entity_type_manager, EntityTypeBundleInfoInterface $entity_type_bundle_info, SectionManager $section_manager, RendererInterface $renderer, AccountInterface $current_user, MessengerInterface $messenger) {
+  public function __construct(ModuleHandlerInterface $module_handler, EntityTypeManagerInterface $entity_type_manager, EntityTypeBundleInfoInterface $entity_type_bundle_info, SectionManager $section_manager, RendererInterface $renderer, AccountInterface $current_user, MessengerInterface $messenger) {
     $this->moduleHandler = $module_handler;
     $this->entityTypeManager = $entity_type_manager;
     $this->entityTypeBundleInfo = $entity_type_bundle_info;
@@ -75,6 +77,21 @@ abstract class BaseSubpageManager {
     $this->renderer = $renderer;
     $this->currentUser = $current_user;
     $this->messenger = $messenger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('module_handler'),
+      $container->get('entity_type.manager'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('ghi_sections.manager'),
+      $container->get('renderer'),
+      $container->get('current_user'),
+      $container->get('messenger'),
+    );
   }
 
 }
