2ed67159984620ed7b9520c563cb4494ef777c48
berliner <berliner@dev030.com>
2024-06-20 12:53:33 +0200

    HPC-9372: The title of plan cluster logframe subpages should always use the current label of the plan cluster it is associated to

--- html/modules/custom/ghi_subpages/src/Entity/LogframeSubpage.php
+++ html/modules/custom/ghi_subpages/src/Entity/LogframeSubpage.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\ghi_subpages\Entity;
 
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\ghi_sections\Entity\SectionNodeInterface;
 use Drupal\layout_builder\LayoutEntityHelperTrait;
 
 /**
@@ -19,6 +21,17 @@ class LogframeSubpage extends SubpageNode {
     $section_storage = $logframe_manager->setupLogframePage($this);
     if ($section_storage) {
       $section_storage->save();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preSave(EntityStorageInterface $storage) {
+    $parent = $this->getParentNode();
+    if (!$parent instanceof SectionNodeInterface) {
+      $label = $parent->label() . ' ' . $this->type->entity->id();
+      $this->setTitle($label);
     }
   }
 
