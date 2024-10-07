47b803a0d976e7a0527a82f68c51131e609957af
berliner <berliner@dev030.com>
2024-10-07 16:12:24 +0200

    HPC-9738: Generalise handling of tableselect sticky form actions work on custom bulk form listing pages

--- html/modules/custom/ghi_subpages/src/Form/SubpagesPagesForm.php
+++ html/modules/custom/ghi_subpages/src/Form/SubpagesPagesForm.php
@@ -11,10 +11,8 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Link;
-use Drupal\Core\Render\Element;
 use Drupal\Core\Render\Markup;
 use Drupal\Core\Routing\RedirectDestinationInterface;
-use Drupal\Core\Security\TrustedCallbackInterface;
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\Url;
 use Drupal\ghi_base_objects\Entity\BaseObjectAwareEntityInterface;
@@ -22,6 +20,7 @@ use Drupal\ghi_sections\Entity\Section;
 use Drupal\ghi_sections\Entity\SectionNodeInterface;
 use Drupal\ghi_subpages\SubpageManager;
 use Drupal\ghi_subpages\SubpageTrait;
+use Drupal\hpc_api\Traits\BulkFormTrait;
 use Drupal\layout_builder\LayoutEntityHelperTrait;
 use Drupal\node\NodeInterface;
 use Drupal\node\NodeTypeInterface;
@@ -31,10 +30,11 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Provides a form for managing subpages of a base entity.
  */
-class SubpagesPagesForm extends FormBase implements TrustedCallbackInterface {
+class SubpagesPagesForm extends FormBase {
 
   use SubpageTrait;
   use LayoutEntityHelperTrait;
+  use BulkFormTrait;
 
   /**
    * The date formatter service.
@@ -137,7 +137,6 @@ class SubpagesPagesForm extends FormBase implements TrustedCallbackInterface {
   public function buildForm(array $form, FormStateInterface $form_state, NodeInterface $node = NULL) {
 
     $form['#attached']['library'][] = 'ghi_subpages/admin.subpages_form';
-    $form['#after_build'][] = [self::class, 'afterBuild'];
 
     /** @var \Drupal\ghi_sections\Entity\SectionNodeInterface $node */
     $node = $this->getBaseTypeNode($node);
@@ -317,181 +316,9 @@ class SubpagesPagesForm extends FormBase implements TrustedCallbackInterface {
       ];
     }
 
-    $bulk_form_actions = $this->getBulkFormActions();
-    if (!empty($bulk_form_actions)) {
-      $this->buildBulkForm($form);
-    }
+    $this->buildBulkForm($form, $this->getBulkFormActions());
 
     return $form;
-  }
-
-  /**
-   * After build callback for the form.
-   *
-   * @param array $form
-   *   The form array.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The form state object.
-   *
-   * @return array
-   *   The form array.
-   */
-  public static function afterBuild(array $form, FormStateInterface $form_state) {
-    foreach (Element::children($form) as $element_key) {
-      $element = &$form[$element_key];
-      if (empty($element['#type']) || $element['#type'] != 'tableselect') {
-        continue;
-      }
-
-      $element['#pre_render'][] = [self::class, 'tableselectPreRender'];
-    }
-    return $form;
-  }
-
-  /**
-   * Pre render callback for tableselect form elements used in the form.
-   *
-   * Add a class to the checkbox column of each row, so that the logic in
-   * core/themes/claro/js/tableselect.js can find the checkboxes.
-   *
-   * @param array $element
-   *   The element array.
-   *
-   * @return array
-   *   The element array.
-   */
-  public static function tableselectPreRender($element) {
-    foreach ($element['#rows'] as &$row) {
-      $row['data'][0] = [
-        'data' => $row['data'][0],
-        'class' => 'subpages-bulk-form',
-      ];
-    }
-    return $element;
-  }
-
-  /**
-   * Build the bulk form.
-   *
-   * Do this in a way that plays nice with Gin.
-   *
-   * @param array $form
-   *   The form array.
-   */
-  private function buildBulkForm(array &$form) {
-    // Build the bulk form. This is mainly done in a way to be compatible with
-    // the gin theme, see gin_form_alter() and gin/styles/base/_views.scss.
-    $form['#prefix'] = Markup::create('<div class="view-content"><div class="views-form">');
-    $form['#suffix'] = Markup::create('</div></div>');
-    $form['header'] = [
-      '#type' => 'container',
-      '#id' => 'edit-header',
-      'subpages_bulk_form' => [
-        '#type' => 'container',
-        '#id' => 'edit-node-bulk-form',
-        'action' => [
-          '#type' => 'select',
-          '#title' => $this->t('Action'),
-          '#options' => $this->getBulkFormActions(),
-        ],
-        'actions' => [
-          '#type' => 'actions',
-          'submit' => [
-            '#type' => 'submit',
-            '#name' => 'bulk_submit',
-            '#value' => $this->t('Apply to selected items'),
-          ],
-        ],
-      ],
-    ];
-
-    self::imitateViewBulkForm($form, 'subpages_bulk_form', $this->t('Section subpages'));
-  }
-
-  /**
-   * Pretend to be a views bulk form.
-   *
-   * This has been copied 1:1 from claro_form_alter, which applies this logic
-   * only to views forms unfortunately.
-   * Because a strict copy it's been added here as a separate function.
-   *
-   * @param array $form
-   *   The form array.
-   * @param string $key
-   *   The string key of the bulk actions form.
-   * @param string|\Drupal\Component\Render\MarkupInterface $view_title
-   *   The pretended title of the view.
-   */
-  private static function imitateViewBulkForm(array &$form, $key, $view_title) {
-    // Move the bulk actions form from the header to its own container.
-    $form['bulk_actions_container'] = $form['header'][$key];
-    unset($form['header'][$key]);
-
-    // Remove the supplementary bulk operations submit button as it appears
-    // in the same location the form was moved to.
-    unset($form['actions']);
-
-    $form['bulk_actions_container']['#attributes']['data-drupal-views-bulk-actions'] = '';
-    $form['bulk_actions_container']['#attributes']['class'][] = 'views-bulk-actions';
-    $form['bulk_actions_container']['actions']['submit']['#button_type'] = 'primary';
-    $form['bulk_actions_container']['actions']['submit']['#attributes']['class'][] = 'button--small';
-    $label = t('Perform actions on the selected items in the %view_title view', ['%view_title' => $view_title]);
-    $label_id = $key . '_group_label';
-
-    // Group the bulk actions select and submit elements, and add a label
-    // that makes the purpose of these elements more clear to
-    // screen readers.
-    $form['bulk_actions_container']['#attributes']['role'] = 'group';
-    $form['bulk_actions_container']['#attributes']['aria-labelledby'] = $label_id;
-    $form['bulk_actions_container']['group_label'] = [
-      '#type' => 'container',
-      '#markup' => $label,
-      '#attributes' => [
-        'id' => $label_id,
-        'class' => ['visually-hidden'],
-      ],
-      '#weight' => -1,
-    ];
-
-    // Add a status label for counting the number of items selected.
-    $form['bulk_actions_container']['status'] = [
-      '#type' => 'container',
-      '#markup' => t('No items selected'),
-      '#weight' => -1,
-      '#attributes' => [
-        'class' => [
-          'js-views-bulk-actions-status',
-          'views-bulk-actions__item',
-          'views-bulk-actions__item--status',
-          'js-show',
-        ],
-        'data-drupal-views-bulk-actions-status' => '',
-      ],
-    ];
-
-    // Loop through bulk actions items and add the needed CSS classes.
-    $bulk_action_item_keys = Element::children($form['bulk_actions_container'], TRUE);
-    $bulk_last_key = NULL;
-    $bulk_child_before_actions_key = NULL;
-    foreach ($bulk_action_item_keys as $bulk_action_item_key) {
-      if (!empty($form['bulk_actions_container'][$bulk_action_item_key]['#type'])) {
-        if ($form['bulk_actions_container'][$bulk_action_item_key]['#type'] === 'actions') {
-          // We need the key of the element that precedes the actions
-          // element.
-          $bulk_child_before_actions_key = $bulk_last_key;
-          $form['bulk_actions_container'][$bulk_action_item_key]['#attributes']['class'][] = 'views-bulk-actions__item';
-        }
-
-        if (!in_array($form['bulk_actions_container'][$bulk_action_item_key]['#type'], ['hidden', 'actions'])) {
-          $form['bulk_actions_container'][$bulk_action_item_key]['#wrapper_attributes']['class'][] = 'views-bulk-actions__item';
-          $bulk_last_key = $bulk_action_item_key;
-        }
-      }
-    }
-
-    if ($bulk_child_before_actions_key) {
-      $form['bulk_actions_container'][$bulk_child_before_actions_key]['#wrapper_attributes']['class'][] = 'views-bulk-actions__item--preceding-actions';
-    }
   }
 
   /**
@@ -680,15 +507,6 @@ class SubpagesPagesForm extends FormBase implements TrustedCallbackInterface {
     return [
       'publish' => $this->t('Publish'),
       'unpublish' => $this->t('Unpublish'),
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function trustedCallbacks() {
-    return [
-      'tableselectPreRender',
     ];
   }
 
---
+++ html/modules/custom/hpc_api/src/Traits/BulkFormTrait.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Drupal\hpc_api\Traits;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\Element;
+use Drupal\Core\Render\Markup;
+
+/**
+ * Provide a simple in-memory cache.
+ */
+trait BulkFormTrait {
+
+  /**
+   * Build the bulk form.
+   *
+   * Do this in a way that plays nice with Gin.
+   *
+   * @param array $form
+   *   The form array.
+   * @param array $bulk_form_actions
+   *   An array of actions as simple key - label pairs.
+   */
+  protected function buildBulkForm(array &$form, array $bulk_form_actions) {
+    if (empty($bulk_form_actions)) {
+      return;
+    }
+
+    $form['#after_build'][] = [self::class, 'afterBuild'];
+
+    // Build the bulk form. This is mainly done in a way to be compatible with
+    // the gin theme, see gin_form_alter() and gin/styles/base/_views.scss.
+    $form['#prefix'] = Markup::create('<div class="view-content"><div class="views-form">');
+    $form['#suffix'] = Markup::create('</div></div>');
+    $form['header'] = [
+      '#type' => 'container',
+      '#id' => 'edit-header',
+      'subpages_bulk_form' => [
+        '#type' => 'container',
+        '#id' => 'edit-node-bulk-form',
+        'action' => [
+          '#type' => 'select',
+          '#title' => $this->t('Action'),
+          '#options' => $bulk_form_actions,
+        ],
+        'actions' => [
+          '#type' => 'actions',
+          'submit' => [
+            '#type' => 'submit',
+            '#name' => 'bulk_submit',
+            '#value' => $this->t('Apply to selected items'),
+          ],
+        ],
+      ],
+    ];
+
+    self::imitateViewBulkForm($form, 'subpages_bulk_form', $this->t('Section subpages'));
+  }
+
+  /**
+   * Pretend to be a views bulk form.
+   *
+   * This has been copied 1:1 from claro_form_alter, which applies this logic
+   * only to views forms unfortunately.
+   * Because a strict copy it's been added here as a separate function.
+   *
+   * @param array $form
+   *   The form array.
+   * @param string $key
+   *   The string key of the bulk actions form.
+   * @param string|\Drupal\Component\Render\MarkupInterface $view_title
+   *   The pretended title of the view.
+   */
+  private static function imitateViewBulkForm(array &$form, $key, $view_title) {
+    // Move the bulk actions form from the header to its own container.
+    $form['bulk_actions_container'] = $form['header'][$key];
+    unset($form['header'][$key]);
+
+    // Remove the supplementary bulk operations submit button as it appears
+    // in the same location the form was moved to.
+    unset($form['actions']);
+
+    $form['bulk_actions_container']['#attributes']['data-drupal-views-bulk-actions'] = '';
+    $form['bulk_actions_container']['#attributes']['class'][] = 'views-bulk-actions';
+    $form['bulk_actions_container']['actions']['submit']['#button_type'] = 'primary';
+    $form['bulk_actions_container']['actions']['submit']['#attributes']['class'][] = 'button--small';
+    $label = t('Perform actions on the selected items in the %view_title view', ['%view_title' => $view_title]);
+    $label_id = $key . '_group_label';
+
+    // Group the bulk actions select and submit elements, and add a label
+    // that makes the purpose of these elements more clear to
+    // screen readers.
+    $form['bulk_actions_container']['#attributes']['role'] = 'group';
+    $form['bulk_actions_container']['#attributes']['aria-labelledby'] = $label_id;
+    $form['bulk_actions_container']['group_label'] = [
+      '#type' => 'container',
+      '#markup' => $label,
+      '#attributes' => [
+        'id' => $label_id,
+        'class' => ['visually-hidden'],
+      ],
+      '#weight' => -1,
+    ];
+
+    // Add a status label for counting the number of items selected.
+    $form['bulk_actions_container']['status'] = [
+      '#type' => 'container',
+      '#markup' => t('No items selected'),
+      '#weight' => -1,
+      '#attributes' => [
+        'class' => [
+          'js-views-bulk-actions-status',
+          'views-bulk-actions__item',
+          'views-bulk-actions__item--status',
+          'js-show',
+        ],
+        'data-drupal-views-bulk-actions-status' => '',
+      ],
+    ];
+
+    // Loop through bulk actions items and add the needed CSS classes.
+    $bulk_action_item_keys = Element::children($form['bulk_actions_container'], TRUE);
+    $bulk_last_key = NULL;
+    $bulk_child_before_actions_key = NULL;
+    foreach ($bulk_action_item_keys as $bulk_action_item_key) {
+      if (!empty($form['bulk_actions_container'][$bulk_action_item_key]['#type'])) {
+        if ($form['bulk_actions_container'][$bulk_action_item_key]['#type'] === 'actions') {
+          // We need the key of the element that precedes the actions
+          // element.
+          $bulk_child_before_actions_key = $bulk_last_key;
+          $form['bulk_actions_container'][$bulk_action_item_key]['#attributes']['class'][] = 'views-bulk-actions__item';
+        }
+
+        if (!in_array($form['bulk_actions_container'][$bulk_action_item_key]['#type'], ['hidden', 'actions'])) {
+          $form['bulk_actions_container'][$bulk_action_item_key]['#wrapper_attributes']['class'][] = 'views-bulk-actions__item';
+          $bulk_last_key = $bulk_action_item_key;
+        }
+      }
+    }
+
+    if ($bulk_child_before_actions_key) {
+      $form['bulk_actions_container'][$bulk_child_before_actions_key]['#wrapper_attributes']['class'][] = 'views-bulk-actions__item--preceding-actions';
+    }
+  }
+
+  /**
+   * After build callback for the form.
+   *
+   * @param array $form
+   *   The form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state object.
+   *
+   * @return array
+   *   The form array.
+   */
+  public static function afterBuild(array $form, FormStateInterface $form_state) {
+    foreach (Element::children($form) as $element_key) {
+      $element = &$form[$element_key];
+      if (empty($element['#type']) || $element['#type'] != 'tableselect') {
+        continue;
+      }
+
+      $element['#pre_render'][] = function (array $element) {
+        // Add a class to the checkbox column of each row, so that the logic in
+        // core/themes/claro/js/tableselect.js can find the checkboxes.
+        foreach ($element['#rows'] as &$row) {
+          $row['data'][0] = [
+            'data' => $row['data'][0],
+            'class' => 'subpages-bulk-form',
+          ];
+        }
+        return $element;
+      };
+    }
+    return $form;
+  }
+
+}
