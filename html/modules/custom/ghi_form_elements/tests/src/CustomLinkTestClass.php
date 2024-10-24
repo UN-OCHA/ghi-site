03cc8252c3c25bfca452280a1b7a74a2da1c0b24
berliner <berliner@dev030.com>
2024-10-24 10:50:31 +0200

    HPC-9736-tests: Add some unit tests

---
+++ html/modules/custom/ghi_form_elements/tests/src/CustomLinkTestClass.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\Tests\ghi_form_elements;
+
+use Drupal\ghi_form_elements\Traits\CustomLinkTrait;
+
+/**
+ * Test class only to use the trait conveniently.
+ */
+class CustomLinkTestClass {
+
+  use CustomLinkTrait;
+
+}
