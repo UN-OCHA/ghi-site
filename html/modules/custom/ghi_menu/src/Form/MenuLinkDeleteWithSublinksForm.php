6d6a5408eed76d1ba1d45b7ab80a67568271456e
Aymen ESSID <aymen.essid@hpc.tools>
2025-05-12 12:32:31 +0200

    HPC-9952: First delete the parent link, then the sublinks.

--- html/modules/custom/ghi_menu/src/Form/MenuLinkDeleteWithSublinksForm.php
+++ html/modules/custom/ghi_menu/src/Form/MenuLinkDeleteWithSublinksForm.php
@@ -41,31 +41,57 @@ class MenuLinkDeleteWithSublinksForm extends ContentEntityDeleteForm {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $child_ids = [];
     $entity = $this->getEntity();
     $enabled_menus = $this->configFactory->get('ghi_menu.settings')->get('enabled_menus') ?? [];
 
+    // First, collect sublinks IDs.
     if ($entity instanceof MenuLinkContent && in_array($entity->getMenuName(), $enabled_menus)) {
-      $this->deleteSublinks($entity);
+      $child_ids = $this->collectSublinksRecursively($entity->getPluginId(), $entity->getMenuName());
     }
 
+    // Let Drupal handle parent deletion.
     parent::submitForm($form, $form_state);
+
+    // Delete the sublinks if any.
+    if (!empty($child_ids)) {
+      $this->deleteSublinksByIds($child_ids);
+    }
   }
 
   /**
-   * Recursively deletes all sublinks of a given link.
+   * Recursively collects all sublinks IDs of a given link.
    */
-  protected function deleteSublinks(MenuLinkContent $link): void {
+  protected function collectSublinksRecursively(string $plugin_id, string $menu_name): array {
     $storage = $this->entityTypeManager->getStorage('menu_link_content');
-    $sublinks = $storage->loadByProperties([
-      'menu_name' => $link->getMenuName(),
-      'parent' => $link->getPluginId(),
-    ]);
+    $query = $storage->getQuery()
+      ->condition('menu_name', $menu_name)
+      ->condition('parent', $plugin_id)
+      ->accessCheck(FALSE);
+    $sublinks_ids = $query->execute();
 
-    foreach ($sublinks as $sublink) {
-      if ($sublink instanceof MenuLinkContent) {
-        $this->deleteSublinks($sublink);
-        $sublink->delete();
+    $all_sublinks_ids = [];
+    if ($sublinks_ids) {
+      $all_sublinks_ids = $sublinks_ids;
+      foreach ($sublinks_ids as $id) {
+        $child = $storage->load($id);
+        if ($child) {
+          $all_sublinks_ids = array_merge($all_sublinks_ids, $this->collectChildIdsRecursive($child->getPluginId(), $menu_name));
+        }
       }
+    }
+    return $all_sublinks_ids;
+  }
+
+  /**
+   * Deletes all sublinks of a given link.
+   */
+  protected function deleteSublinksByIds(array $ids) {
+    $storage = $this->entityTypeManager->getStorage('menu_link_content');
+    $entities = $storage->loadMultiple($ids);
+
+    if (!empty($entities)) {
+      $storage->delete($entities);
     }
   }
 
