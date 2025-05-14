4eacf198c75501dc87a668e7b1b402691d26248f
berliner <berliner@dev030.com>
2025-05-15 00:49:39 +0200

    HPC-10184: Refactor needs review flag actions for better code re-use

--- html/modules/custom/ghi_content/src/Plugin/Action/NeedsReviewUnflag.php
+++ html/modules/custom/ghi_content/src/Plugin/Action/NeedsReviewUnflag.php
@@ -2,14 +2,6 @@
 
 namespace Drupal\ghi_content\Plugin\Action;
 
-use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Action\ActionBase;
-use Drupal\Core\Cache\Cache;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\ghi_content\Entity\ContentReviewInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 /**
  * Custom action to unset the needs review flag on a node.
  *
@@ -19,39 +11,13 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   type = "node"
  * )
  */
-class NeedsReviewUnflag extends ActionBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($configuration, $plugin_id, $plugin_definition);
-  }
+class NeedsReviewUnflag extends NeedsReviewActionBase {
 
   /**
    * {@inheritdoc}
    */
   public function execute($node = NULL) {
-    if (!$node || !$node instanceof ContentReviewInterface) {
-      return;
-    }
-    $node->needsReview(FALSE);
-    $node->setNewRevision(FALSE);
-    $node->setSyncing(TRUE);
-    $node->save();
-    Cache::invalidateTags($node->getCacheTags());
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function access($object, ?AccountInterface $account = NULL, $return_as_object = FALSE) {
-    if (!$object instanceof ContentReviewInterface) {
-      return $return_as_object ? FALSE : AccessResult::forbidden();
-    }
-    /** @var \Drupal\Core\Access\AccessResultInterface $result */
-    $result = $object->access('update', $account, TRUE);
-    return $return_as_object ? $result : $result->isAllowed();
+    $this->doExecute($node, FALSE);
   }
 
 }
