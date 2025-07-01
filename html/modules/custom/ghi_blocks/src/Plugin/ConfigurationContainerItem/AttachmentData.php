e0e15b41d000223ad7f07d98014f3a5eb6e6655f
berliner <berliner@users.noreply.github.com>
2025-07-01 22:55:04 +0200

    HPC-10231: Improve updating of context objects when applying page config via template or manually, for attachment data configuration container items, look at the current base object too and not only at the current plan

--- html/modules/custom/ghi_blocks/src/Plugin/ConfigurationContainerItem/AttachmentData.php
+++ html/modules/custom/ghi_blocks/src/Plugin/ConfigurationContainerItem/AttachmentData.php
@@ -252,6 +252,10 @@ class AttachmentData extends ConfigurationContainerItemPluginBase {
 
     /** @var \Drupal\ghi_plans\Entity\Plan $plan */
     $plan = $this->getContextValue('plan_object');
+
+    /** @var \Drupal\ghi_base_objects\Entity\BaseObjectInterface $base_object */
+    $base_object = $this->getContextValue('base_object');
+
     if (!$attachment) {
       $errors[] = $this->t('No attachment configured');
     }
@@ -261,6 +265,10 @@ class AttachmentData extends ConfigurationContainerItemPluginBase {
     elseif ($attachment->getPlanId() != $plan->getSourceId()) {
       $errors[] = $this->t('Configured attachment is not available in the context of the current plan');
     }
+    elseif ($base_object && $attachment->getSourceEntity()->id() != $base_object->getSourceId()) {
+      $errors[] = $this->t('Configured attachment is not available in the context of the current base object');
+    }
+
     return $errors;
   }
 
