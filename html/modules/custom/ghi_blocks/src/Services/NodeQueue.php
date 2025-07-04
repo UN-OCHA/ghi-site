7ba37515d5a2cb1972cd309851e746cb32c6ef6c
berliner <berliner@users.noreply.github.com>
2025-07-04 13:51:27 +0200

    HPC-10035: Update id type on existing elements

--- html/modules/custom/ghi_blocks/src/Services/NodeQueue.php
+++ html/modules/custom/ghi_blocks/src/Services/NodeQueue.php
@@ -2,47 +2,10 @@
 
 namespace Drupal\ghi_blocks\Services;
 
-use Drupal\Core\Database\Connection;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Queue\QueueFactory;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
-
 /**
  * Service class for configuration updates of plugins.
  */
-class NodeQueue {
-
-  use StringTranslationTrait;
-
-  /**
-   * The entity type manager service.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * The database.
-   *
-   * @var \Drupal\Core\Database\Connection
-   */
-  protected $database;
-
-  /**
-   * The queue factory.
-   *
-   * @var \Drupal\Core\Queue\QueueFactory
-   */
-  protected $queueFactory;
-
-  /**
-   * Public constructor.
-   */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, Connection $database, QueueFactory $queue_factory) {
-    $this->entityTypeManager = $entity_type_manager;
-    $this->database = $database;
-    $this->queueFactory = $queue_factory;
-  }
+class NodeQueue extends BaseQueue {
 
   /**
    * Queue nodes for updates to the plugin configuration.
