bf5dd7f9d8e558d3d2a34843cc52845722ae1464
berliner <berliner@users.noreply.github.com>
2025-09-23 10:44:05 +0200

    HPC-10333: Fix issue with homepage year switcher

--- html/modules/custom/ghi_blocks/src/Controller/AjaxBlockController.php
+++ html/modules/custom/ghi_blocks/src/Controller/AjaxBlockController.php
@@ -3,6 +3,7 @@
 namespace Drupal\ghi_blocks\Controller;
 
 use Drupal\Component\Plugin\Exception\ContextException;
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\OpenModalDialogCommand;
 use Drupal\Core\Ajax\ReplaceCommand;
@@ -68,40 +69,47 @@ class AjaxBlockController extends ControllerBase implements ContainerInjectionIn
       return $this->sendErrorResponse();
     }
 
-    $node = $block_instance->getPageNode();
-    $contexts = $block_instance->getContexts();
-    $contexts['layout_builder.entity'] = EntityContext::fromEntity($node);
-
-    // Try to find the section component to which the block belongs. If that
-    // works, we can use the SectionComponentBuildRenderArrayEvent to have the
-    // content build, instead of calling ::build directly on the block plugin.
-    // This will assure that all process hooks are called as for the original
-    // build of the block, thus containing all admin links too.
-    $sections = $this->getEntitySections($node);
-    $section_component = NULL;
-    foreach ($sections as $section) {
-      foreach ($section->getComponents() as $component) {
-        $plugin = $component->getPlugin();
-        if (!$plugin instanceof GHIBlockBase) {
-          continue;
-        }
-        $plugin_uuid = $plugin->getUuid() ?? $component->getUuid();
-        if ($plugin_uuid != $block_uuid) {
-          continue;
-        }
-        $section_component = $component;
-      }
-    }
-
     $build = NULL;
-    if ($section_component) {
-      try {
-        $event = new SectionComponentBuildRenderArrayEvent($section_component, $contexts, FALSE);
-        $this->eventDispatcher->dispatch($event, LayoutBuilderEvents::SECTION_COMPONENT_BUILD_RENDER_ARRAY);
-        $build = $event->getBuild();
+    $selector = '.block-' . Html::getClass($block_instance->getPluginId()) . ' > .block-content';
+
+    $node = $block_instance->getPageNode();
+    if ($node) {
+      // This is for node based pages, e.g. sections. It would not work for
+      // page manager pages.
+      $contexts = $block_instance->getContexts();
+      $contexts['layout_builder.entity'] = EntityContext::fromEntity($node);
+
+      // Try to find the section component to which the block belongs. If that
+      // works, we can use the SectionComponentBuildRenderArrayEvent to have the
+      // content build, instead of calling ::build directly on the block plugin.
+      // This will assure that all process hooks are called as for the original
+      // build of the block, thus containing all admin links too.
+      $sections = $this->getEntitySections($node);
+      $section_component = NULL;
+      foreach ($sections as $section) {
+        foreach ($section->getComponents() as $component) {
+          $plugin = $component->getPlugin();
+          if (!$plugin instanceof GHIBlockBase) {
+            continue;
+          }
+          $plugin_uuid = $plugin->getUuid() ?? $component->getUuid();
+          if ($plugin_uuid != $block_uuid) {
+            continue;
+          }
+          $section_component = $component;
+        }
       }
-      catch (ContextException $e) {
-        // Just fail silently.
+
+      if ($section_component) {
+        try {
+          $event = new SectionComponentBuildRenderArrayEvent($section_component, $contexts, FALSE);
+          $this->eventDispatcher->dispatch($event, LayoutBuilderEvents::SECTION_COMPONENT_BUILD_RENDER_ARRAY);
+          $build = $event->getBuild();
+          $selector = '.ghi-block-' . $block_instance->getUuid();
+        }
+        catch (ContextException $e) {
+          // Just fail silently.
+        }
       }
     }
     if (!$build) {
@@ -109,7 +117,6 @@ class AjaxBlockController extends ControllerBase implements ContainerInjectionIn
       $build = $block_instance->build();
     }
 
-    $selector = '.ghi-block-' . $block_instance->getUuid();
     $ajax_response = new AjaxResponse();
     $ajax_response->addCommand(new ReplaceCommand($selector, $build));
     return $ajax_response;
