ea31873445437897302450e8fe906b407eccbd33
berliner <berliner@users.noreply.github.com>
2026-02-11 12:35:54 +0100

    HPC-10363: Replace contact attachments by contact objects

---
+++ html/modules/custom/ghi_plans/src/ApiObjects/Contact.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\ghi_plans\ApiObjects;
+
+use Drupal\ghi_base_objects\ApiObjects\BaseObject;
+
+/**
+ * Abstraction class for API contact objects.
+ */
+class Contact extends BaseObject {
+
+  /**
+   * Define the dimension items used in queries.
+   */
+  const GRAPHQL_ITEMS = [
+    'Id',
+    'Name',
+    'Email',
+    'LeadAgency',
+  ];
+
+  /**
+   * Map the raw data.
+   *
+   * @return object
+   *   An object with the mapped data.
+   */
+  protected function map() {
+    $data = $this->getRawData();
+    return (object) [
+      'id' => $data->Id,
+      'name' => $data->Name,
+      'mail' => $data->Email,
+      'agency' => $data->LeadAgency,
+    ];
+  }
+
+}
