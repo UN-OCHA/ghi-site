41de1eeac8f3ef964fa74e6ef9368790ddaeb38f
berliner <berliner@users.noreply.github.com>
2026-02-17 17:56:25 +0100

    HPC-10363: Update requirements on saving of plan base object

--- html/modules/custom/ghi_plans/src/Entity/Plan.php
+++ html/modules/custom/ghi_plans/src/Entity/Plan.php
@@ -35,6 +35,14 @@ class Plan extends BaseObject implements BaseObjectMetaDataInterface, BaseObject
   /**
    * {@inheritdoc}
    */
+  public function save() {
+    $this->updateRequirements();
+    return parent::save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function toUrl($rel = 'canonical', array $options = []) {
     // Allow to link to FTS.
     if ($rel == 'fts_summary') {
@@ -277,7 +285,12 @@ class Plan extends BaseObject implements BaseObjectMetaDataInterface, BaseObject
       }
     }
     elseif ($this->useProjectRequirements()) {
-      // @todo Implement project requirements.
+      $project_query = $this->getProjectQuery();
+      $projects = $project_query->getProjectsForPlan($this);
+      $requirements = 0;
+      foreach ($projects as $project) {
+        $requirements += $project->getRequirements();
+      }
     }
     $this->get('field_requirements')->setValue($requirements);
   }
